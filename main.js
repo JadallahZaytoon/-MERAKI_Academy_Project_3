0900955e334b8fdf2635a3720557fb6250b94157
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-14 23:13:26 +0300

    Editing getAnArticleById

--- main.js
+++ main.js
@@ -1,64 +1,65 @@
 const express = require("express");
 const app = express();
-const PORT=5000;
-const { v4: uuidv4 } = require('uuid');
+const PORT = 5000;
+const { v4: uuidv4 } = require("uuid");
 app.use(express.json());
 
 const articles = [
-    {
+  {
     id: 1,
-    title: 'How I learn coding?',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    {
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
     id: 2,
-    title: 'Coding Best Practices',
-    description:
-    'Lorem, ipsum dolor sit, Quam, mollitia.',
-    author: 'Besslan',
-    },
-    {
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
     id: 3,
-    title: 'Debugging',
-    description:
-    'Lorem, Quam, mollitia.',
-    author: 'Jouza',
-    },
-    ];
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
 
-
-app.get("/articles",(req,res)=>{
-
-
-res.status(200);
-res.json(articles);
-
+app.get("/articles", (req, res) => {
+  res.status(200);
+  res.json(articles);
 });
 
-app.get(`/articles/search_2`,(req,res)=>{
+app.get(`/articles/search_2`, (req, res) => {
+  const articleId = req.query.id;
 
+  const found = articles.filter((element) => {
+    return element.id == articleId;
+  });
+
+  if (found) {
     res.status(200);
-    const articleId = req.query.id;
-    res.json(articles[articleId-1]);
+    res.json(found);
+  } else {
+    res.status(404);
+    res.json("User not found");
+  }
+});
 
- });
+app.post("/articles", (req, res) => {
+  const newArticel = {
+    id: uuidv4(),
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+  };
+  articles.push(newArticel);
+  res.status(201);
+  res.json(newArticel);
+});
 
- app.post("/articles",(req,res)=>{
-   
-    
-   const newArticel ={id:uuidv4(),title:req.body.title,description:req.body.description,author:req.body.author};
-    articles.push(newArticel);
-    res.status(201);
-    res.json(newArticel);
- });
-
-
-
-app.listen(PORT,()=>{
-
-console.log(`Server works at port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server works at port ${PORT}`);
 });
 
 // {
