6596afda770bda33d237329421c0e07649d062e6
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-28 16:30:36 +0300

    Working on 4.getAllArticles

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,12 +1,23 @@
 import { React, useEffect, useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-
+import axios from "axios";
 export default function Dashboard() {
+
+  const getArticles=()=>{
+
+    axios
+    .get("http://localhost:5000/articles")
+    .then((result))
+  
+  }
+
+
+
   return (
     <>
       <div className="body">
         <div className="articlesBody"></div>
-        <button>Get All Articles</button>
+        <button onClick={getArticles}>Get All Articles</button>
       </div>
     </>
   );
